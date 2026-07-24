9743f8777c6c606dd2bf30c16565d98575d5d7b1
Maria Eduarda Batista Guarim De Arruda - SENAI - Cuiaba/MT <00588525@alunosenai.mt>
2026-07-24 11:28:13 -0400

    as coisinha ai ó

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ nome= input()
 num = int(input("Digite um número para ver a tabuada: "))
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
+
+import random
+
+opcoes = ["Cara", "Coroa"]
+resultado = random.choice(opcoes)
+print(f"O resultado do lançamento foi: {resultado}")
+
+
+nome = ()
+
+
+print("Olá, mundo!")
---
+++ main2.py
@@ -0,0 +1,5 @@
+nome = ()
+
+num = int(input("Digite um número para ver a tabuada: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
